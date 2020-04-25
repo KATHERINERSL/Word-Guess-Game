054f3673e89220db960522aa815198373b7b9052
katherine Lundy <katherinersl@gmail.com>
2020-04-25 16:09:26 -0400

    completed adding functions to js game. continuing on css

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,24 +2,58 @@ var underscoresLabel = document.querySelector("#underscores");
 var remainingLabel = document.querySelector("#remaining-guesses");
 var attemptsLabel = document.querySelector("#attempts");
 
-wordBank = ["enterprise", "data", "spock", "kirk"];
-attempts = []
-
+var wordBank = ["enterprise", "data", "spock", "kirk"];
+var attempts = []
 var remainingAttempts = 10;
 
-function updateLabels() {
-    underscoresLabel.textContent = "_ _ L _ _"
-    remainingLabel.textContent = "11"
-    attemptsLabel.textContent = attempts.join(', ')
+// Main Functionality Starts
+var wordToGuess = getWord();
+updateLabels()
+
+function getWord() {
+    var wordsSize = wordBank.length;
+    var indexWord = Math.floor(Math.random() * wordsSize);
+    
+    return wordBank[indexWord];
 }
 
-updateLabels()
+function updateLabels() {
+    underscoresLabel.textContent = updateBlanks()
+    remainingLabel.textContent = remainingAttempts;
+    attemptsLabel.textContent = attempts.toString();
+}
+
+function updateBlanks() {
+    var blanks = []
+
+    // Go through each letter in wordToGuess
+    for(var letter of wordToGuess) {
+        if(attempts.includes(letter)) {
+            blanks.push(letter)
+        } else {
+            blanks.push('_')
+        }
+    }
+
+    return blanks.join(' ')
+}
 
 document.addEventListener("keypress", function(e) {
     var keyPressed = e.key.toLocaleLowerCase();
-    attempts.push(keyPressed)
 
-    updateLabels()
+    if(!attempts.includes(keyPressed)) {
+        attempts.push(keyPressed);
+
+        if(!wordToGuess.includes(keyPressed)) {
+            remainingAttempts--;
+        }
+    }
+
+    // Decrease attempts by one when key pressed
+    updateLabels();
+
+    if(remainingAttempts == 0) {
+        alert("Game Over!")
+        window.location.reload(true);
+    }
 })
-
-
